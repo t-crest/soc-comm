a0b20c7a27b98649e605b17e19cc076a84247ebf
Martin Schoeberl <martin@jopdesign.com>
2019-09-17 20:17:19 +0200

    s4noc: latency tests with tester2

--- src/test/scala/s4noc/MeasureLatency.scala
+++ src/test/scala/s4noc/MeasureLatency.scala
@@ -1,6 +1,8 @@
 package s4noc
 
 import chisel3._
+import chisel3.tester._
+
 
 // shall go
 import chisel3.iotesters.PeekPokeTester
@@ -23,4 +25,22 @@ object MeasureLatency extends App {
   for (i <- 2 until 5) {
     iotesters.Driver.execute(Array[String](), () => new S4noc(4, i, i, 32)) { c => new MeasureLatency(c) }
   }
+
+  def myTest(dut: S4noc): Unit = {
+    println("in myTest")
+    dut.io.cpuPorts(0).wrData.poke("hcafebabe".U)
+    dut.io.cpuPorts(0).addr.poke(0.U)
+    dut.io.cpuPorts(0).wr.poke(true.B)
+    dut.clock.step(1)
+
+    for (i <- 0 until 20) {
+      println(dut.io.cpuPorts(3).rdData.peek().litValue())
+      dut.clock.step(1)
+    }
+  }
+
+  for (i <- 2 until 5) {
+    RawTester.test(new S4noc(4, i, i, 32)) { myTest }
+  }
+
 }
