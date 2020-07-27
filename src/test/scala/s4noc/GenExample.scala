6d615af17f4c8117df89af59f748ec4811fb1bf1
Martin Schoeberl <martin@jopdesign.com>
2020-07-27 19:05:04 +0200

    s4noc: exmaple uses experimental stuff

--- src/test/scala/s4noc/GenExample.scala
+++ src/test/scala/s4noc/GenExample.scala
@@ -5,6 +5,7 @@ package s4noc
 import chisel3._
 import chisel3.tester._
 import org.scalatest._
+import chisel3.experimental.BundleLiterals._
 
 class GenExample extends FlatSpec with ChiselScalatestTester with Matchers {
   behavior of "GenExample"
@@ -27,15 +28,8 @@ class GenExample extends FlatSpec with ChiselScalatestTester with Matchers {
   it should "work" in {
     test(new M(UInt(16.W))) { c =>
 
-      // Following does not run
-      val e = Wire(new Elem(UInt(16.W)))
-      e.data := 1.asUInt(16.W)
-      e.time := 2.asUInt
-      c.io.in.poke(e)
-
-      // .Lit does not work in the following, even not on 0.2-SNAPSHOT
-      // c.io.in.poke(chiselTypeOf(c.io.in).Lit(_.data -> 1.U, _.time -> 2.U))
-
+      // this works now :-)
+      c.io.in.poke(chiselTypeOf(c.io.in).Lit(_.data -> 1.U, _.time -> 2.U))
 
       c.clock.step(1)
       c.io.out.data.expect(1.U)
