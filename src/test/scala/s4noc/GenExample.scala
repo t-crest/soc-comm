37ce028d6c05eeedaaf790328d932b8af74ac4f8
Martin Schoeberl <martin@jopdesign.com>
2020-07-30 17:06:44 +0200

    s4noc: variations how to poke a bundle

--- src/test/scala/s4noc/GenExample.scala
+++ src/test/scala/s4noc/GenExample.scala
@@ -13,6 +13,11 @@ class GenExample extends FlatSpec with ChiselScalatestTester with Matchers {
   class Elem[T <: Data](private val dt: T) extends Bundle {
     val data = dt.cloneType
     val time = UInt(8.W)
+
+    def myPoke(d: T, t: UInt): Unit = {
+      data.poke(d)
+      time.poke(t)
+    }
   }
 
   class M[T <: Data](dt: T) extends Module {
@@ -22,17 +27,35 @@ class GenExample extends FlatSpec with ChiselScalatestTester with Matchers {
     })
 
     io.out.data := io.in.data
-    io.out.time := 2.U
+    io.out.time := io.in.time + 1.U
+
   }
 
-  it should "work" in {
+  it should "work with an experimental bundle literal" in {
     test(new M(UInt(16.W))) { c =>
 
-      // this works now :-)
       c.io.in.poke(chiselTypeOf(c.io.in).Lit(_.data -> 1.U, _.time -> 2.U))
-
       c.clock.step(1)
       c.io.out.data.expect(1.U)
+    }
+  }
+
+  it should "work with helper function" in {
+    test(new M(UInt(16.W))) { c =>
+
+      c.io.in.myPoke(3.U, 4.U)
+      c.clock.step(1)
+      c.io.out.data.expect(3.U)
+    }
+  }
+
+  it should "work directly" in {
+    test(new M(UInt(16.W))) { c =>
+
+      c.io.in.data.poke(13.U)
+      c.io.in.time.poke(4.U)
+      c.clock.step(1)
+      c.io.out.data.expect(13.U)
     }
   }
 }
--- src/test/scala/s4noc/NITester.scala
+++ src/test/scala/s4noc/NITester.scala
@@ -1,39 +1,26 @@
-/*
- * Copyright: 2017, Technical University of Denmark, DTU Compute
- * Author: Martin Schoeberl (martin@jopdesign.com)
- * License: Simplified BSD License
- */
-
 package s4noc
 
 import chisel3._
 import chisel3.tester._
 import org.scalatest._
-
-
-/**
- * Test the router by printing out the value at each clock cycle
- * and checking some known end values.
- */
+import chisel3.experimental.BundleLiterals._
 
 class NITester extends FlatSpec with ChiselScalatestTester with Matchers {
   behavior of "NI"
 
   it should "work" in {
     test(new NetworkInterface(4, 2, 2, UInt(16.W))) { dut =>
-      // TODO: how can I create a an Entry of generic type to be used in testing?
-      // Or how do I create a bundle literal that contains an Entry with a generic type?
 
-      // Following does not run
-      val e = Wire(new Entry(UInt(16.W)))
-      e.data := 1.asUInt(16.W)
-      e.time := 2.asUInt
-      dut.io.networkPort.tx.bits.poke(e)
+      dut.io.networkPort.tx.bits.poke(chiselTypeOf(dut.io.networkPort.tx.bits).Lit(_.data -> 1.U, _.time -> 2.U))
+      dut.io.networkPort.tx.valid.poke(true.B)
 
-      // .Lit does not work in the following, even not on 0.2-SNAPSHOT
-      // dut.io.networkPort.tx.bits.poke(chiselTypeOf(dut.io.networkPort.tx.bits).Lit(1.U))
-
-      dut.clock.step(1)
+      for (i <- 1 until 30) {
+        println(dut.io.networkPort.tx.ready.peek.litToBoolean)
+        println(dut.io.local.out.data.peek.litValue)
+        dut.clock.step()
+        dut.io.networkPort.tx.valid.poke(false.B)
+      }
+      dut.clock.step(10)
       // may take more than one clock cycle...
       dut.io.local.out.data.expect(1.U)
     }
