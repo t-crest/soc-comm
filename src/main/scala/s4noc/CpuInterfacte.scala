bea10bdc7f278c476b9fe32c9828a98e58e10c52
Martin Schoeberl <martin@jopdesign.com>
2020-09-10 17:51:40 +0200

    s4noc: towards a CPU interface

--- src/main/scala/s4noc/CpuInterfacte.scala
+++ src/main/scala/s4noc/CpuInterfacte.scala
@@ -5,9 +5,91 @@ import chisel3.util._
 
 import soc._
 
+/**
+  * CPU interface to two ready/valid channels.
+  *
+  *
+  * TODO: maybe this is generic and belongs into soc?
+  * TODO: maybe NetworkPort should then have a different name?
+  */
 class CpuInterfacte extends Module {
   val io = IO(new Bundle {
     val cpuPort = new IOPort(4)
     val networkPort = Flipped(new NetworkPort(UInt(32.W)))
   })
+
+  val cp = io.cpuPort
+  val tx = io.networkPort.tx
+  val rx = io.networkPort.rx
+
+  val idle :: read :: writeWait :: Nil = Enum(3)
+  val stateReg = RegInit(idle)
+
+  val rdyReg = RegInit(false.B)
+  // TODO: how do I get the type of the entry?
+  val dataReg = Reg(UInt(32.W))
+  val addrReg = Reg(UInt(8.W))
+
+  cp.rdy := rdyReg
+
+  // some default values to make it compile
+  cp.rdData := 0.U
+  rx.ready := false.B
+
+  // write
+  tx.valid := false.B
+  tx.bits.data := Mux(stateReg === idle, cp.wrData, dataReg)
+  tx.bits.time := Mux(stateReg === idle, cp.address, addrReg)
+  tx.valid := cp.wr || stateReg === writeWait
+
+
+  rdyReg := false.B
+  switch(stateReg) {
+    is(idle) {
+      when(cp.wr) {
+        when (tx.ready) {
+          rdyReg := true.B
+        } .otherwise {
+          dataReg := cp.wrData
+          addrReg := cp.address
+          stateReg := writeWait
+        }
+      }
+    }
+    is (read) {
+
+    }
+    is (writeWait) {
+      when (tx.ready) {
+        rdyReg := true.B
+        stateReg := idle
+      }
+    }
+  }
+
+
+  /*
+  This is the combinational version
+  when (io.cpuPort.wr && io.networkPort.tx.ready) {
+    io.networkPort.tx.valid := true.B
+  }
+
+   */
+
+}
+
+
+object CpuInterface extends App {
+
+  /*
+  (new chisel3.stage.ChiselStage).emitVerilog(new CpuInterfacte())
+   */
+
+  /*
+  (new chisel3.stage.ChiselStage).execute(Array("-X", "Verilog", "--target-dir", "generated"),
+    Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new CpuInterfacte())))
+*/
+
+  chisel3.Driver.execute(Array("--target-dir", "generated"),
+    () => new CpuInterfacte())
 }
---
+++ src/test/scala/s4noc/CpuInterfaceTester.scala
@@ -0,0 +1,54 @@
+
+package s4noc
+
+import chisel3._
+import chiseltest._
+import chiseltest.experimental.TestOptionBuilder._
+import org.scalatest._
+
+class CpuInterfaceTester extends FlatSpec with ChiselScalatestTester with Matchers {
+  behavior of "The CpuInterface"
+
+  it should "handle write timing" in {
+    test(new CpuInterfacte).withAnnotations(Seq(chiseltest.internal.WriteVcdAnnotation)) {
+      d => {
+
+        def step() = d.clock.step()
+
+        val cpu = d.io.cpuPort
+        val noc = d.io.networkPort
+        step()
+        cpu.rdy.expect(false.B)
+
+        // maybe set some default values
+
+        // single cycle ready
+        cpu.address.poke(0.U)
+        cpu.wrData.poke(0x0123.U)
+        cpu.wr.poke(true.B)
+        noc.tx.ready.poke(true.B)
+        step()
+        noc.tx.bits.data.expect(0x0123.U)
+        cpu.rdy.expect(true.B)
+        // delayed ready
+        cpu.wr.poke(false.B)
+        cpu.wrData.poke(0x0.U)
+        step()
+        noc.tx.ready.poke(false.B)
+        cpu.wrData.poke(0x456.U)
+        cpu.wr.poke(true.B)
+        step()
+        cpu.wr.poke(false.B)
+        cpu.wrData.poke(0x0.U)
+        cpu.rdy.expect(false.B)
+        step()
+        cpu.rdy.expect(false.B)
+        step()
+        noc.tx.ready.poke(true.B)
+        noc.tx.bits.data.expect(0x0456.U)
+        step()
+        cpu.rdy.expect(true.B)
+      }
+    }
+  }
+}
