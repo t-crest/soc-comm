e58c6c8c27cf818f3e0e0210082a64498bd86b8a
Martin Schoeberl <martin@jopdesign.com>
2022-09-20 18:37:48 +0200

    s4noc: user traffik generator in tester

--- src/test/scala/s4noc/PerformanceTest.scala
+++ src/test/scala/s4noc/PerformanceTest.scala
@@ -7,23 +7,58 @@ import scala.collection.mutable._
 
 class PerformanceTest extends AnyFlatSpec with ChiselScalatestTester {
 
+  val n = 2
   "S4NoC" should "be fast ;-)" in {
-    test(new Network(2, UInt(32.W))) { d =>
-      val sched = Schedule(2)
+    test(new Network(n, UInt(32.W))) { d =>
+
+      var countCycles = 0
+      def tick() = {
+        d.clock.step()
+        countCycles += 1
+      }
+
+      val sched = Schedule(n)
       println(sched)
-      for (i <- 0 until 4) {
+      for (i <- 0 until n * n) {
         println(s"From core $i")
         for (j <- 0 until sched.schedule.length) {
-          val dest = sched.timeToDest(i, j)
-          println(s"  at timeslot $j it reaches $dest")
+          val dest = sched.timeToDest(i, j)._1
+          println(s"  at timeslot $j core $i reaches $dest")
         }
-        for (j <- 0 until 4) {
+        for (j <- 0 until n * n) {
           val slot = sched.coreToTimeSlot(i, j)
           println(s"  use slot $slot to reach code $j")
         }
       }
 
-      val t = new TrafficGen(2)
+      def printInfo(data: Int) = {
+        val from = data >> 24
+        val to = (data >> 16) & 0x0ff
+        val cnt = data & 0x0ffff
+        if (data == -1) {
+          println("Have -1")
+        } else {
+          println(s"Have from $from to $to inserted at $cnt")
+        }
+      }
+
+      val t = new TrafficGen(n * n)
+      var slotCnt = 0
+      for (i <- 0 until 10) {
+        t.tick()
+        for (core <- 0 until n * n) {
+          val dest = sched.timeToDest(core, slotCnt)._1
+          println(s"$slotCnt: core $core can send to $dest")
+          if (dest != -1) {
+            val data = t.getValue(core, dest)
+            printInfo(data)
+          }
+        }
+        
+        tick()
+        slotCnt = (slotCnt + 1) % sched.len
+        println(s"slot #: $slotCnt")
+      }
 
     }
   }
--- src/test/scala/s4noc/TrafficGen.scala
+++ src/test/scala/s4noc/TrafficGen.scala
@@ -3,6 +3,10 @@ package s4noc
 import scala.collection.mutable
 import scala.util.Random
 
+/**
+  *
+  * @param n all cores (not n x n)
+  */
 class TrafficGen(n: Int) {
 
   val queues = Array.ofDim[mutable.Queue[Int]](n, n)
@@ -12,16 +16,28 @@ class TrafficGen(n: Int) {
     }
   }
 
-  def insert(from: Int, to: Int, at: Int): Unit = {
-    queues(from)(to).enqueue(at)
+  def insert(from: Int, to: Int, data: Int): Unit = {
+    queues(from)(to).enqueue(data)
+  }
+
+  def getValue(from: Int, to: Int): Int = {
+    val q = queues(from)(to)
+    if (q.isEmpty) {
+      -1
+    } else {
+      q.dequeue()
+    }
   }
 
   var countCycles = 0
   var inserted = 0
-  var injectionRate = 0.1 * n
+  var injectionRate = 0.5 * n
+
+  // This would be a direct call back into the Chisel tester
+  // def tick(inject: (Int, Int) => Unit): Unit = {
 
   /**
-    * Execute this once per clock cycle
+    * Execute this once per clock cycle and call back for packet injection
     */
   def tick(): Unit = {
     // remember which source and destination was taken in a cycle
@@ -32,25 +48,44 @@ class TrafficGen(n: Int) {
 
     def getOne(s: mutable.Set[Int]): Int = {
       var one = r.nextInt(n)
+      var count = 0
       while (s.contains(one)) {
         one = r.nextInt(n)
+        if (count > n) throw new Exception("Livelock in getOne()")
+        count += 1
       }
       s += one
       one
     }
-    countCycles += 1
-    while (inserted.toDouble / countCycles < injectionRate) {
+
+    while (inserted.toDouble / (countCycles + 1) < injectionRate) {
       inserted += 1
       val from = getOne(fromSet)
       var to = getOne(toSet)
       if (to == from) to = getOne(toSet)
       println(s"$countCycles $inserted: $from -> $to")
+      insert(from, to, (from << 24) | (to << 16) | countCycles)
     }
+
+    countCycles += 1
   }
 }
 
 object TrafficGen extends App {
-  val t = new TrafficGen(8)
-  for (i <- 0 until 100) t.tick()
+
+  val n = 4
+  val t = new TrafficGen(n)
+
+  for (i <- 0 until 10) {
+    t.tick()
+    for (i <- 0 until n) {
+      for (j <- 0 until n) {
+        val data = t.getValue(i, j)
+        if (data != -1) {
+          println(s"At $data $i -> $j")
+        }
+      }
+    }
+  }
 }
 
