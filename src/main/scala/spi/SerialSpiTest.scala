c0836fd7a35d38c66aefaf131268e7b88dd15f29
Martin Schoeberl <martin@jopdesign.com>
2024-11-17 23:09:25 -0800

    spi: fix Java based serial IO

--- src/main/scala/spi/SerialSpiTest.scala
+++ src/main/scala/spi/SerialSpiTest.scala
@@ -7,28 +7,24 @@ class SerialSpiTest {
 }
 
 object SerialSpiTest extends App {
-  val ports = SerialPort.getCommPorts
-  for (port <- ports) {
-    println(port.getSystemPortName)
-    println(port.getDescriptivePortName)
-    println(port.getManufacturer)
-  }
   // TODO: fix this hard coded thing
-  val port = ports(3)
+  val port = SerialPort.getCommPort("/dev/tty.usbserial-210292B408601")
   port.openPort()
   port.setBaudRate(115200)
   // port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0)
   port.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 0, 0)
   val out = port.getOutputStream
   val buf = new Array[Byte](1)
+  val s = "Hello World"
   for (i <- 0 until 10) {
-    out.write('0' + i)
+    val v = s(i)
+    out.write(v)
     Thread.sleep(100)
-    println(s"Wrote ${'0' + i}")
+    println(s"Wrote ${v.toChar}")
     if (port.bytesAvailable() != 0) {
       println(s"Bytes available: ${port.bytesAvailable()}")
       val cnt = port.readBytes(buf, 1)
-      println("read: " + cnt + " " + buf(0))
+      println("read: " + cnt + " " + (buf(0).toChar))
     }
   }
   out.close()
